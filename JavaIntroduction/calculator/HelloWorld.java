c062a71e9cefc652774223d04eb553cce0623179
Keshav Saharia <keshav@keshavsaharia.com>
2013-06-16 18:29:02 -0700

    added calculator

---
+++ JavaIntroduction/calculator/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * The computer gods,
+ * spoke upon man: "hello world"
+ * when it all began.
+ * 
+ * @author (your name)
+ */
+public class HelloWorld
+{
+    /**
+     * When you tell a Java class to execute, it will first try to start from the main function. The
+     * header below, "public static void main", is the standard header for the starting point of Java
+     * programs.
+     */
+    public static void main(String[] args)
+    {
+        // Write your code here!
+    }
+}
